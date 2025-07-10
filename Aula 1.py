6347562700b670a250d2dab69fc8b5bec4f09996
Kalilsje <kalil.jereissati101@gmail.com>
2025-07-10 20:12:53 -0300

    att aula 1

--- Aula 1.py
+++ Aula 1.py
@@ -1,5 +1,24 @@
-print("hello world!\n" "Ola iury")
+print("hello world!\n" "Ola iury");
 
 x = 2345
 print(x)
-print(type(x))
+print(type(x));
+
+#Operadores aritméticos
+
+x = 5
+y = 2
+
+print(x // y);
+print(x + y);
+print(x - y);
+print(x % y);
+print(x / y);
+print(x * y);
+print(x ** y);
+
+#Entrada do usuário
+print("Olá, qual seu nome?")
+nome = input("digite seu nome: ")
+print(nome);
+
