2b60083fad9dae99cd7d852c45feb9c80bbf8495
Kalilsje <kalil.jereissati101@gmail.com>
2025-07-11 15:30:39 -0300

    aula 1 att

--- Quiz.py
+++ Quiz.py
@@ -2,15 +2,14 @@ pontuacao = 0
 Opcao = input("Qual arma é vendida a 2700 no lado TR do CS?\n" "a) M4A4\n"  "b) FAMAS\n" "c) Ak-47\n" "d) M4A1-S\n");
 if Opcao == "c":
     print("Resposta Certa")
-    pontuacao =+ 1
-else:
+    pontuacao += 1
     print("Resposta Errada")
 
 
 Escolha = input("Qual skin é da raridade exótica no CS?\n" "a) Dragon Lore\n"  "b) Howl\n" "c) Medusa\n" "d) Fire Serpent\n");
 if Escolha == "b":
     print("Resposta Certa")
-    pontuacao =+ 2
+    pontuacao += 1
 else:
     print("Resposta Errada")
 
@@ -18,8 +17,8 @@ else:
 Decisao = input("Qual Caixa se dropa a Fire Serpent no CS? \n" "a) Bravo\n"  "b) Revolver\n" "c) Hydra\n" "d) Aperto\n");
 if Decisao == "a":
     print("Resposta Certa")
-    pontuacao =+ 3
+    pontuacao += 1
 else:
     print("Resposta Errada")
 
-print("Sua pontuacao final foi de: ", pontuacao);
+print("Sua pontuacao final foi de: ", pontuacao, "pontos.");
--- Triangulo.py
+++ Triangulo.py
@@ -1,30 +1,14 @@
-Lados = 0
 LadoA = input("Tamanho do primeiro lado: ");
 LadoB = input("Tamanho do segundo lado: ");
 LadoC = input("Tamanho do terceiro lado: ");
 LadoA = int(LadoA)
 LadoB = int(LadoB)
 LadoC = int(LadoC)
-Lados = int(Lados)
 
-if LadoA < (LadoB + LadoC):
-    print("É um triângulo");
-else:
-    print("Não é um triângulo");
-
-if LadoB < (LadoA + LadoC):
-    print("É um triângulo");
-else:
-    print("Não é um triângulo");
-
-if LadoC < (LadoA + LadoB):
-    print("É um triângulo");
-else:
-    print("Não é um triângulo");
-
-if LadoA == LadoB == LadoC:
-    print("É um triângulo equilátero");
-elif LadoA != LadoB != LadoC:
-    print("É um triângulo escaleno");
-else:
-    print("É um triângulo isósceles");
+if LadoA < (LadoB + LadoC) or LadoB < (LadoA + LadoC) or LadoC < (LadoA + LadoB):
+    if LadoA == LadoB == LadoC:
+        print("É um triângulo equilátero");
+    elif LadoA != LadoB != LadoC:
+        print("É um triângulo escaleno");
+    else:
+        print("É um triângulo isósceles");
