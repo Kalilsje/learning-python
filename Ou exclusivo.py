cfd8054b1c636c2418a62150135925d06197ff00
Kalilsje <kalil.jereissati101@gmail.com>
2025-07-11 15:49:39 -0300

    att aula1 dia 11

---
+++ Ou exclusivo.py
@@ -0,0 +1,7 @@
+print("Digite 1 para verdeiro e 0 para Falso");
+x = bool(int(input("Primeiro Valor: ")));
+y = bool(int(input("Segundo Valor: ")));
+if x == y:
+    print("True")
+else:
+    print("False")
