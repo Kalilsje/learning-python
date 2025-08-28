f5b64131c593f0a7b48114c536de3ed1c8704164
Kalil Sousa <kalil.jereissati101@gmail.com>
2025-08-28 20:46:36 -0300

    att PATHLIB

--- PATHLIB/main.py
+++ PATHLIB/main.py
@@ -1,3 +1,13 @@
 from pathlib import Path
 diret = Path("folder1")
-print(diret.exists())
+#diret = diret.absolute()
+#print(diret.root)
+#print(diret.parent)
+#print(list(diret.parents))
+#print(diret.parts)
+#diret = diret / "test22.txt"
+#print(diret.touch())
+#diret.unlink()
+
+diret = diret / "Text1.txt"
+print(diret.read_text())
